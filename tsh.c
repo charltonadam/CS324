848825cde373aca6f330237a261ae1ef51d3d51e
Adam-Desktop <charltonadam@live.com>
2018-02-02 18:49:23 -0700

    no message

--- tsh.c
+++ tsh.c
@@ -307,9 +307,16 @@ int builtin_cmd(char **argv)
     } else if(strcmp(argv[0], bg) == 0) {
       //TODO: implement % for job IDs
 
-      struct job_t* updater = getjobjid(jobs, atoi(argv[1]));
+      struct job_t* updater;
 
-      if(updater->state != ST) {
+      if(argv[1][0] == '%') {
+        //background getjob
+        updater = getjobjid(jobs, atoi(argv[1]));
+      } else {
+        updater = getjobpid(jobs, atoi(argv[1]));
+      }
+
+      if(updater == NULL || updater->state != ST) {
         //error
       } else {
         updater->state = BG;
@@ -319,8 +326,16 @@ int builtin_cmd(char **argv)
       return 1;
     } else if(strcmp(argv[0], fg) == 0) {
 
-      struct job_t* updater = getjobjid(jobs, atoi(argv[1]));
-      if(updater->state != ST || updater->state != BG) {
+      struct job_t* updater;
+
+      if(argv[1][0] == '%') {
+        //background getjob
+        updater = getjobjid(jobs, atoi(argv[1]));
+      } else {
+        updater = getjobpid(jobs, atoi(argv[1]));
+      }
+
+      if(updater == NULL || updater->state != ST || updater->state != BG) {
         //error
       } else {
         updater->state = FG;
